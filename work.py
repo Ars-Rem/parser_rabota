396356aed11b137e07ebf45465efffeed6024bea
Ars-Rem <67710184+Ars-Rem@users.noreply.github.com>
2021-02-04 19:33:31 +0200

    Update work.py

--- work.py
+++ work.py
@@ -9,13 +9,37 @@ if sys.platform == 'linux':
 else:
     import os
 
+
+class Buttons:
+    def __init__(self, button=0):
+        self.button = button
+
+    def push(self):
+        if self.button == 1:
+            URL = 'https://rabota.ua/zapros/python/львов/pg1'
+            return URL
+        elif self.button == 2:
+            URL = 'https://rabota.ua/zapros/trainee/украина/pg1'
+            return URL
+        else:
+            URL = 'https://rabota.ua/львов/pg1'
+            return URL
+try:
+    s = Buttons(int(input('1==python/львов, 2==trainee/украина.\n Сделать выбор:')))
+except ValueError:
+    s = Buttons(0)
+#  print(s.push())
+
+# URL = 'https://rabota.ua/zapros/trainee/украина/pg1'
+# URL = 'https://rabota.ua/zapros/python/львов/pg1'
 # URL = 'https://rabota.ua/jobsearch/vacancy_list?keyWords=Python&regionId=0'
-URL = 'https://rabota.ua/zapros/python/львов/pg1'
+
+
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/83.0.4103.116 Safari/537.36', 'accept': '*/*'}
 HOST = 'https://rabota.ua'
 
-FILE ="{}_work.csv".format(datetime.now())
+FILE = "{}work.csv".format(datetime.now())
 
 
 def get_url(url, params=None):
@@ -25,10 +49,14 @@ def get_url(url, params=None):
 
 def get_pages_count(html):
     soup = BeautifulSoup(html, 'html.parser')
-    page = soup('dd', class_='f-always-blue')
+    page = soup('dd', class_='')
+    print(page)
+
     if page:
+        print(int(page[-1].get_text()))
         return int(page[-1].get_text())
     else:
+        print(1)
         return 1
 
 
@@ -50,7 +78,6 @@ def get_data(html):
     return work
 
 
-#  TODO:problem with path from win10
 def save(items, path):
     with open(path, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
@@ -61,7 +88,7 @@ def save(items, path):
 
 
 def parse():
-    html = get_url(URL)
+    html = get_url(s.push())
     if html.status_code == 200:
         print('Соединение с сервером установлено')
         work = []
@@ -69,15 +96,16 @@ def parse():
         pages_count = get_pages_count(html.text)
         for i in range(1, pages_count + 1):
             print(f'Получение данных со страницы {i} из {pages_count} страниц')
-            html = get_url(URL, params={'pg': i})
+            html = get_url(s.push(), params={'pg': i})
             work.extend(get_data(html.text))
             save(work, FILE)
         print(f'найдено {len(work)} вакансий')
         if sys.platform == 'linux':
-            opener = "open"
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
             subprocess.call([opener, FILE])
+
         else:
-            os.startfile(os.path.normpath(FILE))
+            os.startfile(FILE)
     else:
         print("Error connection from site")
 
